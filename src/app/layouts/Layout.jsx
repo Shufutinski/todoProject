26a91456059610a9c3d451f4df08db530f51da0e
Shufutinski <yanarsaev93@gmail.com>
2023-01-05 16:49:16 +0300

    refactor: create menu component and move code from layout into it

--- src/app/layouts/Layout.jsx
+++ src/app/layouts/Layout.jsx
@@ -1,15 +1,12 @@
 import React from 'react';
+import Menu from '../components/Menu';
 
 const Layout = () => {
   const brand = 'ToDo';
 
   return (
     <>
-      <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
-        <div className="container-fluid justify-content-center">
-          <a className="navbar-brand mb-1 h1">{brand}</a>
-        </div>
-      </nav>
+      <Menu brand={brand} />
     </>
   )
 }
